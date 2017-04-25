b761f7912c367fffcf91b031c996371619a4af51
JoVsn <vilsaint.jordan@gmail.com>
2017-04-25 12:19:07 +0200

    New files

---
+++ src/functions.c
@@ -0,0 +1,21 @@
+#include "ball.h"
+#include "bar.h"
+#include "brick.h"
+#include "geometry.h"
+
+int collisionsBetweenBalls() {
+	return 0;
+}
+
+int collisionBallBrick() {
+	return 0;
+}
+
+int collisionBallBar(Ball ball, Bar bar) {
+
+	if (bar.position.y > 0) {
+		Vector myVector = VectorXY();
+	}
+
+	return 0;
+}
